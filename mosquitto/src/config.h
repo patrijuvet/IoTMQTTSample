d05e353ce8f0f0ce92095434de103f52429659ff
patrijuvet <55324094+patrijuvet@users.noreply.github.com>
2024-07-21 09:02:14 -0400

    Update config.h

--- mosquitto/src/config.h
+++ mosquitto/src/config.h
@@ -1,8 +1,8 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-#define IOTHUBNAME "{iothub_name}"
-#define DEVICEID   "{device_id}"
-#define SAS_TOKEN  "{sas_token}"
+#define IOTHUBNAME "PatIOThub"
+#define DEVICEID   "Pat_V1"
+#define SAS_TOKEN  "SharedAccessSignature sr=PatIOThub.azure-devices.net%2Fdevices%2FPat_V1&sig=IxtoAdEbnZAqjq1cGUFU219LOImOthCbSM6uZKIJMt4%3D&se=1721571537"
 
 #define CERTIFICATEFILE CERT_PATH "IoTHubRootCA.crt.pem"
